58861b3c4f5dc067e52ad1d218ed0a19c06e9438
olhazamlynska <olya.sonechko@gmail.com>
2022-10-19 15:45:38 +0300

    Update start filles

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { PixabayAPI } from './js/PixabayAPI';
